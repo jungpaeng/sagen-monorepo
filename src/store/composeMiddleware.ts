5fe954ee057675c52610f423feeb012fa5f47c5a
jungpaeng <meis1541@naver.com>
2021-03-30 14:06:13 +0900

    fix: composeMiddleware 코드 최적화

--- src/store/composeMiddleware.ts
+++ src/store/composeMiddleware.ts
@@ -5,18 +5,23 @@ type MiddlewareAPI<State = any> = {
   getState: CreateStore<State>['getState'];
   setState: CreateStore<State>['setState'];
 };
-type Middleware<State = any> = (api: MiddlewareAPI<State>) => StoreEnhancer<State>;
+export type Middleware<State = any> = (api: MiddlewareAPI<State>) => StoreEnhancer<State>;
 
 export const composeMiddleware = function <State = any>(...middlewares: Middleware<State>[]) {
   return function (createStore: StoreEnhancerStoreCreator<State>) {
     return function (defaultState: State) {
       const store = createStore(defaultState);
-      const middlewareChain = middlewares.map((middleware) =>
-        middleware({ getState: store.getState, setState: store.setState }),
-      );
-      const setState = compose(...middlewareChain)(store.setState);
+      const { getState, setState } = store;
 
-      return { ...store, setState };
+      let customSetState = () => {
+        throw new Error('setState while constructing your middleware is not allowed.');
+      };
+      const middlewareChain = middlewares.map((middleware) =>
+        middleware({ getState, setState: customSetState }),
+      );
+      customSetState = compose(...middlewareChain)(setState);
+
+      return { ...store, setState: customSetState };
     };
   };
 };
