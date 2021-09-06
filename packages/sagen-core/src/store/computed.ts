debbca00d4927ccacfffa3c2db706d971b334ab6
jungpaeng <meis1541@naver.com>
2021-09-06 18:44:01 +0900

    feat[sagen-core]: computed

--- packages/sagen-core/src/store/computed.ts
+++ packages/sagen-core/src/store/computed.ts
@@ -8,17 +8,11 @@ export function computed<Computed = any, State = any>(
   store: CreateStore<State>,
   expression: (state: State) => Computed,
 ): ComputedStore<Computed, State> {
-  let computedStore = expression(store.getState());
-
-  const getWithComputed = () => {
-    return computedStore;
-  };
-
   const setWithComputed: CreateStore<State>['setState'] = (partial) => {
     store.setState(partial);
-
-    const currentStore = store.getState();
-    computedStore = expression(currentStore);
+  };
+  const getWithComputed = () => {
+    return expression(store.getState());
   };
 
   return {
